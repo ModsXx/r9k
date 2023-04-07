ed63cd71c1bbd1fea70bec9259d9025e58bb77c9
ModsXx <118561210+ModsXx@users.noreply.github.com>
2023-04-07 12:19:05 -0500

    Add files via upload

---
+++ assets/preload/stages/naxi.lua
@@ -0,0 +1,7 @@
+function onCreate()
+	-- background shit
+	makeLuaSprite('house', 'shuaiby/naxi', -350, -150);
+	addLuaSprite('house',false)
+        setLuaSpriteScrollFactor('house', 1, 1);
+
+end
---
+++ assets/preload/stages/shuaiby.lua
@@ -0,0 +1,11 @@
+function onCreate()
+	-- background shit
+	makeLuaSprite('house', 'shuaiby/stageback', -350, -150);
+	addLuaSprite('house',false)
+        setLuaSpriteScrollFactor('house', 1, 1);
+
+makeLuaSprite('stair_class', 'shuaiby/stagebox', -350, -150);
+
+	addLuaSprite('stair_class', true);
+
+end
